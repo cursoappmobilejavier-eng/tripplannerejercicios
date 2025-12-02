5da420946a4ad414c34fc8af004ae7fc63739e91
Javier <cursoappmobilejavier@gmail.com>
2025-12-02 13:54:20 +0100

    integrar pagina de guardar

---
+++ tripplannerejercicios/Data/Instrument.swift
@@ -0,0 +1,14 @@
+//
+//  Instrument.swift
+//  TripPlanner
+//
+//  Created by Mananas on 02/12/25.
+//
+
+import Foundation
+
+struct Instrument: Codable {
+    let id: String
+    let marca: String
+    let modelo: String
+}
